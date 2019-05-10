d122834ae54eb826df46e6d6d4b36d9f86792fd4
ksshlee <ksshlee@naver.com>
2019-05-11 00:41:12 +0900

    Merge branch 'master' of /Users/ksshlee/AndroidStudioProjects/WhatCatDo with conflicts.

--- app/src/main/java/com/team_project2/hans/whatcatdo/Classify.java
+++ app/src/main/java/com/team_project2/hans/whatcatdo/Classify.java
@@ -8,7 +8,7 @@ import java.util.List;
 class Classify {
     //happy=1, angry=2, sad=3, curious=4, ignore=5, scared=6, sleepy=7, sad=8
     int image[] = {1,1,3,2,3,3,3,2,1,4,7,5,2,3,1,4,5,2};
-    int accuracy;
+    int accuracy ;
     int result[] = new int[8];
 
     List<Classifier.Recognition> results;
