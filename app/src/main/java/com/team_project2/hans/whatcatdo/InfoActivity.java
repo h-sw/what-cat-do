794c0ae2f763c782f99ac22aff7138ae58eee1f1
SeonHak <tjsgkr134679@naver.com>
2019-05-12 16:35:49 +0900

    도움말 Fragment작성 중 오류발생

--- app/src/main/java/com/team_project2/hans/whatcatdo/InfoActivity.java
+++ app/src/main/java/com/team_project2/hans/whatcatdo/InfoActivity.java
@@ -1,14 +1,64 @@
 package com.team_project2.hans.whatcatdo;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class InfoActivity extends AppCompatActivity {
-
+public class InfoActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bt1,bt2,bt3;
+    FragmentManager fm;
+    FragmentTransaction tran;
+    InfoFragment1 frag1;
+    InfoFragment2 frag2;
+    InfoFragment3 frag3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
+        bt3 = (Button) findViewById(R.id.bt3);
+        bt1.setOnClickListener(this);
+        bt2.setOnClickListener(this);
+        bt3.setOnClickListener(this);
+        frag1 = new InfoFragment1(); //프래그먼트 객채셍성
+        frag2 = new InfoFragment2(); //프래그먼트 객채셍성
+        frag3 = new InfoFragment3(); //프래그먼트 객채셍성
+        setFrag(0); //프래그먼트 교체
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bt1:
+                setFrag(0);
+                break;
+            case R.id.bt2:
+                setFrag(1);
+                break;
+            case R.id.bt3:
+                setFrag(2);
+                break;
+        }
+    }
+    public void setFrag(int n){    //프래그먼트를 교체하는 작업을 하는 메소드를 만들었습니다
+        fm = getFragmentManager();
+        tran = fm.beginTransaction();
+        switch (n){
+            case 0:
+                tran.replace(R.id.main_frame, frag1);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 1:
+                tran.replace(R.id.main_frame, frag2);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 2:
+                tran.replace(R.id.main_frame, frag3);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/team_project2/hans/whatcatdo/InfoFragment2.java
@@ -0,0 +1,19 @@
+package com.team_project2.hans.whatcatdo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoFragment2 extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_info2, container, false);
+
+        return view;
+    }
+}
