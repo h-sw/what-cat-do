3ad07cb6e49f0ca98f134c90af2837e7a331bfd9
SeonHak <tjsgkr134679@naver.com>
2019-05-12 17:57:11 +0900

    fragment를 이용한 도움말 작성

--- app/src/main/java/com/team_project2/hans/whatcatdo/InfoActivity.java
+++ app/src/main/java/com/team_project2/hans/whatcatdo/InfoActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity implements View.OnClickListener{
-    Button bt1,bt2,bt3;
+    Button bt1,bt2,bt3,bt4;
     FragmentManager fm;
     FragmentTransaction tran;
     Fragment frag1;
@@ -23,9 +23,11 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
         bt1 = findViewById(R.id.bt1);
         bt2 = findViewById(R.id.bt2);
         bt3 = findViewById(R.id.bt3);
+        bt4 = findViewById(R.id.bt4);
         bt1.setOnClickListener(this);
         bt2.setOnClickListener(this);
         bt3.setOnClickListener(this);
+        bt4.setOnClickListener(this);
         frag1 = new InfoFragment1(); //프래그먼트 객채셍성
         frag2 = new InfoFragment2(); //프래그먼트 객채셍성
         frag3 = new InfoFragment3(); //프래그먼트 객채셍성
@@ -44,6 +46,8 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.bt3:
                 setFrag(2);
                 break;
+            case R.id.bt4:
+                setFrag(3);
         }
     }
     public void setFrag(int n){    //프래그먼트를 교체하는 작업을 하는 메소드를 만들었습니다
@@ -58,6 +62,9 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case 2:
                 tran.replace(R.id.main_frame, frag3);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                break;
+            case 3:
+                finish();
                 break;
         }
         tran.commit();
