69f2e6399a27cc018e292f879a30bd6c669b2f72
hanseungwoo <hansi100@naver.com>
2019-05-12 16:57:06 +0900

    setContentView 봐라 선학아

--- app/src/main/java/com/team_project2/hans/whatcatdo/InfoActivity.java
+++ app/src/main/java/com/team_project2/hans/whatcatdo/InfoActivity.java
@@ -1,7 +1,8 @@
 package com.team_project2.hans.whatcatdo;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +12,17 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
     Button bt1,bt2,bt3;
     FragmentManager fm;
     FragmentTransaction tran;
-    InfoFragment1 frag1;
-    InfoFragment2 frag2;
-    InfoFragment3 frag3;
+    Fragment frag1;
+    Fragment frag2;
+    Fragment frag3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        bt1 = (Button) findViewById(R.id.bt1);
-        bt2 = (Button) findViewById(R.id.bt2);
-        bt3 = (Button) findViewById(R.id.bt3);
+        //setContentView(R.layout.activity_main); // 메인 엑티비티가 아니라 인포액피비티 자나요!!!!!!!!!!!!!!!!
+        setContentView(R.layout.activity_info);
+        bt1 = findViewById(R.id.bt1);
+        bt2 = findViewById(R.id.bt2);
+        bt3 = findViewById(R.id.bt3);
         bt1.setOnClickListener(this);
         bt2.setOnClickListener(this);
         bt3.setOnClickListener(this);
@@ -29,6 +31,7 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
         frag3 = new InfoFragment3(); //프래그먼트 객채셍성
         setFrag(0); //프래그먼트 교체
     }
+
     @Override
     public void onClick(View v){
         switch (v.getId()){
@@ -44,21 +47,19 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
     public void setFrag(int n){    //프래그먼트를 교체하는 작업을 하는 메소드를 만들었습니다
-        fm = getFragmentManager();
+        fm = getSupportFragmentManager();
         tran = fm.beginTransaction();
         switch (n){
             case 0:
                 tran.replace(R.id.main_frame, frag1);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
                 break;
             case 1:
                 tran.replace(R.id.main_frame, frag2);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
                 break;
             case 2:
                 tran.replace(R.id.main_frame, frag3);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
                 break;
         }
+        tran.commit();
     }
 }
