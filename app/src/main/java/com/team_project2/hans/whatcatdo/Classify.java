e99564c4cdb98d9ef69267df6d40fa0ba3856f00
ksshlee <ksshlee@naver.com>
2019-05-14 13:03:54 +0900

    array를 list로!! 소트도 간편하게!

--- app/src/main/java/com/team_project2/hans/whatcatdo/Classify.java
+++ app/src/main/java/com/team_project2/hans/whatcatdo/Classify.java
@@ -3,27 +3,28 @@ package com.team_project2.hans.whatcatdo;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Classify {
     //happy=1, angry=2, sad=3, curious=4, ignore=5, scared=6, sleepy=7, sad=8
-    ArrayList image = new ArrayList();//이미지의 감정을 저장해주는 리스트
-    int accuracy ;//정확도 값
-    ArrayList result = new ArrayList();//결과 값을 저장해주는 리스트
+    ArrayList<Integer> image = new ArrayList();//이미지의 감정을 저장해주는 리스트
+    int accuracy ;//정확도
+    ArrayList<Integer> result = new ArrayList();//결과 값을 저장해주는 리스트
 
     List<Classifier.Recognition> results;
     public Classify(List<Classifier.Recognition> results) {
         this.results = results;
 
-        for(Classifier.Recognition result : results){
-            Log.d("SSS",Float.toString(result.getConfidence()));
+        for(Classifier.Recognition result : results) {
+            Log.d("SSS", Float.toString(result.getConfidence()));
         }
     }
 
 
-    void classify(){
+    public void classify(){
         for(int i=0;i<18;i++){
-            result.add(image.get(i));
+            result.add(image.get(i),+1);
         }
 
         Collections.sort(result);//result값을 소트 해준다
